3adfed9b25c72dfb39b0f72c9c89e29c454748fd
oscarsangpa <oscarsangrador@gmail.com>
2023-02-01 14:34:33 +0100

    add first test cases

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Jest</h1>
+        <h1>Radio App</h1>
+      <input 
+        placeholder="search radio"
+        type="text" 
+      />
+      <button>Search</button>
+
+      <section aria-label='station-list'>
+
+      </section>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeEach(()=> render(<App/>))
+
+describe("App component render correctly", () => {
+  test("App render correctly", () => {
+    const r = render(<App/>)
+    expect(r).toBeDefined();
+  })
+  test("the name is shown", () => {
+    const name = "Radio App"
+    const el = screen.getByText(name);
+    expect(el).toBeInTheDocument()
+  })
 });
+
+describe("Should be able to search radio by name", () => {
+  test("App should have a placeholder name in the input", () => {
+    const placeholderText = "search radio"
+    const input = screen.getByPlaceholderText(placeholderText)
+    expect(input).toBeInTheDocument();
+  })
+  test("App should have a button to search", () => {
+    const buttonText = "Search"
+    const button = screen.getByText(buttonText)
+    expect(button).toBeInTheDocument();
+  })
+  test("When we click on the button, it is executed only once", () => {
+    const mockFn = jest.fn()
+    const buttonText = "Search"
+    const button = screen.getByText(buttonText)
+    button.addEventListener("click", mockFn)
+    fireEvent.click(button)
+    expect(mockFn).toHaveBeenCalledTimes(1)  
+  })
+})
+
+describe("Radio station list", () => {
+  test("radio station list should exist", () => {
+    const list = screen.getByLabelText("station-list")
+    expect(list).toBeInTheDocument()
+  })
+  text("The radio station list should be started empty", () => {
+    const list = screen.getByLabelText("station-list")
+    const childrenCount = list.childElementCount
+    expect(childrenCount).toBe(0)
+  })
+})
