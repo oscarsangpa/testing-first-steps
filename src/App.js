e053f1606b8c142a3cb3ca26ebd59965c96f2ee7
oscarsangpa <oscarsangrador@gmail.com>
2023-02-06 12:29:00 +0100

    feat: call api with axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState("")
+  const [list, setList] = useState([])
+
+
+  const handleClick = () => {
+    const url = `http://de1.api.radio-browser.info/json/stations/byname/${search}`
+
+    axios
+      .get(url)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
         <h1>Radio App</h1>
       <input 
         placeholder="search radio"
-        type="text" 
+        type="text"
+        value={search}
+        onChange={e => setSearch(e.target.value)}
       />
-      <button>Search</button>
+      <button
+        onClick={handleClick}      
+      >Search</button>
 
       <section aria-label='station-list'>
 
--- src/App.test.js
+++ src/App.test.js
@@ -41,9 +41,20 @@ describe("Radio station list", () => {
     const list = screen.getByLabelText("station-list")
     expect(list).toBeInTheDocument()
   })
-  text("The radio station list should be started empty", () => {
+  test("The radio station list should be started empty", () => {
     const list = screen.getByLabelText("station-list")
     const childrenCount = list.childElementCount
     expect(childrenCount).toBe(0)
+  })
+  test("When a valid search is made it must show at least one result.", () => {
+    const placeholderText = "search radio"
+    const input = screen.getByPlaceholderText(placeholderText)
+    const buttonText = "Search"
+    const button = screen.getByText(buttonText)
+    fireEvent.change(input, { target: { value: "Country" }})
+    fireEvent.click(button)
+    const list = screen.getByLabelText("station-list")
+    const childrenCount = list.childElementCount
+    expect(childrenCount).toBeGreaterThanOrEqual(1)
   })
 })
